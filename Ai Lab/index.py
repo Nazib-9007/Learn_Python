16cceca0b3f2f691f4aa8821e7b294f7ad67b9a7
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-07-28 20:32:01 +0600

    Update index.py
    Added type conversion and multiple variable assignment examples.

--- Ai Lab/index.py
+++ Ai Lab/index.py
@@ -21,3 +21,19 @@ print("This should not run")
 a multiline 
 comment
 """
+# type conversion...
+x = 5;
+y = str(x);
+print (type(x), type(y));
+
+# Many values to multiple variables..
+x, y, z = 'Bangladesh', 'Palestine', 'South Korea';
+print(x, y, z);
+
+x = y = z = 'All are countries';
+print(x);
+
+# Unpack a collection...
+country = ['Bangladesh', 'Palestine', 'South Korea'];
+x, y, z = country;
+print (x, y, z);
