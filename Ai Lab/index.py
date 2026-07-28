0e1de82be5a5429f558edb9eb1970e092db99894
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-07-28 20:10:44 +0600

    Update index.py

--- Ai Lab/index.py
+++ Ai Lab/index.py
@@ -1,2 +1,23 @@
 print('Hello world');
 print('This is my first python code');
+
+print('Hello world', end =' Hi world');
+print ('I am', 25);
+# multiline comment use....
+"""
+This is a comment
+written in 
+more than just one line
+"""
+print ('Hi world');
+
+#Write a single-line comment
+# This is comment
+# Comment out this line so it does not run:
+print("This should not run")
+
+# Add a multiline comment
+"""
+a multiline 
+comment
+"""
