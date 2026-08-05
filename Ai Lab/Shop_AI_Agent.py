b0e60bddd8be9e9ef1622961bd90986e2e9c6127
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-08-05 21:04:12 +0600

    Add files via upload

---
+++ Ai Lab/Shop_AI_Agent.py
@@ -0,0 +1,143 @@
+print("Ai: Welcome To fresh fruits Ai agent");
+customer = input("ME: ");
+
+
+if "hi"in customer or "hello" in customer:
+  print("AI: "+ customer + " dear customer");
+  print("AI: How can I help you today?")
+  print("AI: You can ask about: fruits, prices, offers, or delivery")
+  customer = input("ME: ");
+
+
+  if "fruits" in customer:
+    print("AI: We have Apples, Banana, Oranges, Grapes, Mangos");
+    customer = input("ME: ");
+
+  elif "prices" in customer:
+      print("AI: Here are our prices:")
+      print("AI: Apples: $2.99/kg")
+      print("AI: Bananas: $1.49/kg")
+      print("AI: Oranges: $3.49/kg")
+      print("AI: Grapes: $4.99/kg")
+      print("AI: Mangoes: $3.99/kg")
+      print("Which one you buy?")
+      customer = input("ME: ");
+
+
+  elif "offers" in customer:
+    print("Currently we don't have any offers")
+    print("Which fruits you needs?")
+    customer = input("ME: ");
+
+  elif "do you have delivery process" in customer:
+    print("AI: Sorry currently it's not available");
+
+
+if "Apples" in customer:
+  print("AI: Price will be $2.99")
+  print("AI: any thing else?")
+  customer = input("ME: ");
+  if "yes" in customer or "yeah" in customer:
+    print("AI: what else would you like to know?")
+    customer = input("ME: ");
+    if "prices" in customer:
+        print("AI: Mangos: $3.99/kg, Bananas: $1.49/kg")
+        print("AI: Grapes: $4.99/kg, Oranges: $3.99/kg")
+    elif "offers" in customer:
+              print("AI: Currently we don't have any offers")
+    elif "delivery" in customer:
+              print("AI: Sorry currently it's not available")
+  elif "no" in customer or "bye" in customer or "exit" in customer:
+          print("AI: go to the counter and checkout the products.")
+          customer = input("ME: ");
+          if "bye" in customer or "ok" in customer:
+            print("AI: Thank you for visiting Fresh Fruits!")
+            print("AI: Bye, have a greate day!")
+
+
+if "Bananas" in customer:
+  print("AI: Price will be $1.49")
+  print("AI: any thing else?")
+  customer = input("ME: ");
+  if "yes" in customer or "yeah" in customer:
+    print("AI: what else would you like to know?")
+    customer = input("ME: ");
+    if "prices" in customer:
+        print("AI: Apples: $2.99/kg, Mangos: $3.99/kg")
+        print("AI: Grapes: $4.99/kg, Oranges: $4.99/kg")
+    elif "offers" in customer:
+              print("AI: Currently we don't have any offers")
+    elif "delivery" in customer:
+              print("AI: Sorry currently it's not available")
+  elif "no" in customer or "bye" in customer or "exit" in customer:
+          print("AI: go to the counter and checkout the products.")
+          customer = input("ME: ");
+          if "bye" in customer or "ok" in customer:
+            print("AI: Thank you for visiting Fresh Fruits!")
+            print("AI: Bye, have a greate day!")
+
+
+if "Oranges" in customer:
+  print("AI: Price will be $2.99")
+  print("AI: any thing else?")
+  customer = input("ME: ");
+  if "yes" in customer or "yeah" in customer:
+    print("AI: what else would you like to know?")
+    customer = input("ME: ");
+    if "prices" in customer:
+        print("AI: Apples: $2.99/kg, Bananas: $1.49/kg")
+        print("AI: Grapes: $4.99/kg, Mangos: $3.99/kg")
+    elif "offers" in customer:
+              print("AI: Currently we don't have any offers")
+    elif "delivery" in customer:
+              print("AI: Sorry currently it's not available")
+  elif "no" in customer or "bye" in customer or "exit" in customer:
+          print("AI: go to the counter and checkout the products.")
+          customer = input("ME: ");
+          if "bye" in customer or "ok" in customer:
+            print("AI: Thank you for visiting Fresh Fruits!")
+            print("AI: Bye, have a greate day!")
+
+
+if "Grapes" in customer:
+  print("AI: Price will be $2.99")
+  print("AI: any thing else?")
+  customer = input("ME: ");
+  if "yes" in customer or "yeah" in customer:
+    print("AI: what else would you like to know?")
+    customer = input("ME: ");
+    if "prices" in customer:
+        print("AI: Apples: $2.99/kg, Bananas: $1.49/kg")
+        print("AI: Mangos: $4.99/kg, Oranges: $3.99/kg")
+    elif "offers" in customer:
+              print("AI: Currently we don't have any offers")
+    elif "delivery" in customer:
+              print("AI: Sorry currently it's not available")
+  elif "no" in customer or "bye" in customer or "exit" in customer:
+          print("AI: go to the counter and checkout the products.")
+          customer = input("ME: ");
+          if "bye" in customer or "ok" in customer:
+            print("AI: Thank you for visiting Fresh Fruits!")
+            print("AI: Bye, have a greate day!")
+
+
+if "Mangos" in customer:
+  print("AI: Price will be $2.99")
+  print("AI: any thing else?")
+  customer = input("ME: ");
+  if "yes" in customer or "yeah" in customer:
+    print("AI: what else would you like to know?")
+    customer = input("ME: ");
+    if "prices" in customer:
+        print("AI: Apples: $2.99/kg, Bananas: $1.49/kg")
+        print("AI: Grapes: $4.99/kg, Oranges: $3.99/kg")
+    elif "offers" in customer:
+              print("AI: Currently we don't have any offers")
+    elif "delivery" in customer:
+              print("AI: Sorry currently it's not available")
+  elif "no" in customer or "bye" in customer or "exit" in customer:
+          print("AI: go to the counter and checkout the products.")
+          customer = input("ME: ");
+          if "bye" in customer or "ok" in customer:
+            print("AI: Thank you for visiting Fresh Fruits!")
+            print("AI: Bye, have a greate day!")
