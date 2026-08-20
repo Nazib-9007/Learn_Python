4ed762e48385dc6922b8103bc5527a63b8d577a5
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-08-20 21:37:08 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# Math function...
+import math
+f = 5
+print(f'Factorial of {f}! = {math.factorial(f)}')
+
+a = 15
+b = 5
+print(f'The gcd of {b} and {a} is: {math.gcd(b,a)}');
+
+x = -10
+print(f'The absolute value of {x} is: {math.fabs(x)}')
