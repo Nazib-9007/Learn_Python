5c92f587dbfce0bb931f3ba17461d8bb448478c9
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-08-21 19:35:41 +0600

    Update main.py
    Added logical operator examples for loan eligibility.

--- Ai Lab/main.py
+++ Ai Lab/main.py
@@ -9,3 +9,22 @@ print(f'The gcd of {b} and {a} is: {math.gcd(b,a)}');
 
 x = -10
 print(f'The absolute value of {x} is: {math.fabs(x)}')
+# Logical operator...
+has_high_income = False
+has_good_credit = True
+has_criminal_record = False
+
+#And operator...
+if has_high_income and has_good_credit:
+    print('Eligible for loan')
+else:
+    print('Sorry! you are not eligible for loan')
+
+# OR operator...
+if has_high_income or has_good_credit:
+    print('Eligible for loan');
+else:
+    print('Not eligible for loan');
+
+if has_good_credit and not has_criminal_record:
+    print('Eligible for loan')
